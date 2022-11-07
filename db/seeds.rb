3948f7d7d1e2df1180fb14037be45f963169c271
Isheim <weston.isheim@molex.com>
2022-11-07 10:48:59 -0500

    Adds authenticate_user! to home path

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.create!(email: 'test@example.com', password: 'password', password_confirmation: 'password', superadmin: true) 
+# User.create!(email: 'test@example.com', password: 'password', password_confirmation: 'password', superadmin: true) 
+User.create!(email: 'test2@example.com', password: 'password', password_confirmation: 'password', superadmin: false) 
