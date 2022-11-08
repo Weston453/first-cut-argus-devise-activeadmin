cd94dae4ac208b1a8121b26b877bddc1541ae485
Isheim <weston.isheim@molex.com>
2022-11-08 10:24:54 -0500

    Changes Login field from login to username

--- db/migrate/20221107205559_add_username_to_users.rb
+++ db/migrate/20221107205559_add_username_to_users.rb
@@ -3,7 +3,5 @@ class AddUsernameToUsers < ActiveRecord::Migration[7.0]
     add_column :users, :add_username_to_users, :string
     add_column :users, :username, :string
     add_index :users, :username, unique: true
-    remove_column :users, :email
-    remove_index :users, :email
   end
 end
